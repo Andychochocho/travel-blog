a23371e8071582502e54eea1b60690e75e458264
Matthew Newell and Andrew Cho <cho-andrew@hotmail.com>
2016-04-21 15:11:19 -0700

    create/update/delete locations

--- src/Travel Blog/Controllers/LocationsController.cs
+++ src/Travel Blog/Controllers/LocationsController.cs
@@ -23,5 +23,47 @@ namespace Travel_Blog.Controllers
             thisLocation.Experiences = db.Experiences.Where(experience => experience.LocationId == id).Include(x => x.Peoples).ToList();
             return View(thisLocation);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Location location)
+        {
+            db.Locations.Add(location);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Location location)
+        {
+            db.Entry(location).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            db.Locations.Remove(thisLocation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
